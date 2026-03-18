46562a4fa886051a2e933e72e96590cedd1ff7bf
Johnathan Calhoun <jcalhoun8742@gmail.com>
2026-03-18 16:44:59 -0400

    Update Main.java
    I added a random table method and I created a list of open tables and a list of closed tables per Hunter's instructions.

--- Main.java
+++ Main.java
@@ -1,4 +1,17 @@
 public class Main{
+import java.util.Random;
+    public static MyLinkedList randTbls(int num, boolean open){
+        MyLinkedList tbls = new MyLinkedList();
+        Random rand = new Random();
+        for(int i = 0; i < num; i++){
+            Table tbl = new Table(i + 1, rand.nextInt(8) + 1);
+            if(!open){
+                tbl.occupy();
+            }
+            tbls.add(tbl);
+        }
+    }
+    
     public static void main(String args[]){
         MyLinkedList list = new MyLinkedList();
 
@@ -12,7 +25,11 @@ public class Main{
         list.remove(20);
 
         list.printList();
-
+        
         System.out.println("Contains 30?" + list.contains(30));
+
+        MyLinkedList openTbls = randTbls(40, true);
+        MyLinkedList closeTbls = randTbls(40, false);
+
     }
 }
