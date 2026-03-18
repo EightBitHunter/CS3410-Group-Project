bbdef077969466f0deeefbddda2d0e5742aaa394
joshj2006 <joshua.lamar.jackson@gmail.com>
2026-03-18 17:19:39 -0400

    Update1 Main.java
    / Declare both linked lists
    / Create 40 table nodes
    / Random seat size between 1 - 8

--- Main.java
+++ Main.java
@@ -2,6 +2,18 @@ public class Main{
     public static void main(String args[]){
         MyLinkedList list = new MyLinkedList();
 
+        //create open and closed table lists
+        MyLinkedList<Table> openTables = new MyLinkedList<>();
+        MyLinkedList<Table> closedTables = new MyLinkedList<>();
+
+        Random rand = new Random();
+        //fill openTables with random seats
+        for (int i = 0; i < 40; i++){
+            int seats = rand.nextInt(8) + 1;
+            Table table = new Table(seats);
+            openTables.add(table);
+        }
+
         list.add(10);
         list.add(20);
         list.add(30);
