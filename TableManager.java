4f5553c4f8515cf3011bed4dd3dc7d81d4e50a1d
Johnathan Calhoun <jcalhoun8742@gmail.com>
2026-03-24 13:52:12 -0400

    Update TableManager.java
    Had to change the seatParty method, so we can sit a party larger than 8 to multiple tables, I also made sure that the table numbers were close together.

--- TableManager.java
+++ TableManager.java
@@ -13,17 +13,79 @@ public class TableManager {
 		openTbls.add(t);
 	}
 	
-	public Table seatParty(int size) {
+	public MyLinkedList seatParty(int size) {
+		MyLinkedList readyTbls = new MyLinkedList();
+		
 		for(int i = 0; i < openTbls.size(); i++) {
 			Table t = (Table) openTbls.get(i);
 			
 			if(size <= t.getSize()) {
 				openTbls.remove(t);
 				closeTbls.add(t);
-				return t;
+				readyTbls.add(t);
+				return readyTbls;
 			}
 		}
-		return null;
+		
+		int remain = size;
+		
+		Table max = null;
+		for(int i = 0; i < openTbls.size(); i++) {
+			Table t = (Table) openTbls.get(i);
+			if(max == null || t.getSize() > max.getSize()) {
+				max = t;
+			}
+		}
+		
+		if(max == null) {
+			return null;
+		}
+		
+		openTbls.remove(max);
+		closeTbls.add(max);
+		readyTbls.add(max);
+		remain -= max.getSize();
+		
+		while(remain > 0 && openTbls.size() > 0){
+			
+			Table best = null;
+			int bestDist = Integer.MAX_VALUE;
+			
+			for(int i = 0; i < openTbls.size(); i++) {
+				Table t = (Table) openTbls.get(i);
+				
+				if(t.getSize() >= remain) {
+					int dist = Math.abs(t.getTblNum() - max.getTblNum());
+					if(dist < bestDist) {
+						bestDist = dist;
+						best = t;
+					}
+				}
+			}
+			
+			if(best == null) {
+				Table nxtMax = null;
+				for(int i = 0; i < openTbls.size(); i++) {
+					Table t = (Table) openTbls.get(i);
+					if(nxtMax == null || t.getSize() > nxtMax.getSize()) {
+						nxtMax = t;
+					}
+				}
+				if(nxtMax == null) {
+					break;
+				}
+				
+				best = nxtMax;
+			}
+			openTbls.remove(best);
+			closeTbls.add(best);
+			readyTbls.add(best);
+			remain -= best.getSize();
+			
+			max = best;
+		}
+		
+		return readyTbls;
 	}
 	
 	public void freeTable(int num) {
